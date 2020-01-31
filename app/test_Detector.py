8c7c6929bf9c548ae735d5a3c7859bf7b9c70680
phessabi <parham.hessabi@gmail.com>
2020-01-31 23:21:40 +0330

    Add test for Detector class

---
+++ app/test_Detector.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from Detector import Detector
+
+
+class TestDetector(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        super(TestDetector, cls).setUpClass()
+        cls.model = Detector()
+
+    def testHavingAnswer(self):
+        result = self.model.detect('sag')
+        self.assertIn('answer', result)
+
+    def testHavingSpamWords(self):
+        result = self.model.detect('sag')
+        self.assertIn('spam_words', result)
+
+    def testSpamWordsLength(self):
+        result = self.model.detect('salam sag chetori khubi')
+        spam_words = result['spam_words'].replace(' ', '').split('-')
+        self.assertLess(len(spam_words), 4)
